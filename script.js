b5270fd94456a594bb125e95a1535ea8d80fd8a1
PreOnZi <152984741+PreOnZi@users.noreply.github.com>
2024-02-01 19:01:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function checkPassword() {
+    var password = document.getElementById("password-input").value;
+    // Replace 'yourpassword' with the actual password
+    if (password === "1234567890") {
+        document.body.classList.remove("password-protected");
+        document.getElementById("overlay").style.display = "none";
+    } else {
+        alert("Incorrect password. Please try again.");
+    }
+}
